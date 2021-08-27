801894b3c0f3474e35ceca9fa1105c89488a7966
femanov <femanov@gmail.com>
2021-08-27 14:52:10 +0700

    debugging unit and daemons

--- cservice/demonizers.py
+++ cservice/demonizers.py
@@ -1,44 +1,52 @@
 import daemon
-import signal
+import signalg
 import sys
 import pid
 from datetime import datetime
 import builtins
+from contextlib import redirect_stdout,redirect_stderr
+from systemd.daemon import notify
 
 
 class Service:
     def __init__(self, name, **kwargs):
         self.name = name
+        # argsparse?
+        self.systemd = True if '-systemd' in sys.argv else False
+        self.not_daemonize = True if '-not-daemonize' in sys.argv else False
 
-        # may be argparse later
-        if '-not-daemonize' in sys.argv:
-            print('runnind without forking demonization')
-            self.pre_run()
-            self.main()
-            self.run_main_loop()
-            sys.exit(0)
+        self.detach_process = False if self.systemd or self.not_daemonize else True
 
-        self.f_log = open('/var/tmp/' + self.name + '.log', 'ab', 0)
-        self.f_err = open('/var/tmp/' + self.name + '.err', 'ab', 0)
-        self.dcontext = daemon.DaemonContext(pidfile=pid.PidFile(self.name, '/var/tmp',), stdout=self.f_log, stderr=self.f_err)
+        if self.not_daemonize:
+            self.f_log = sys.stdout
+            self.f_err = sys.stderr
+        else:
+            self.f_log = open('/var/tmp/' + self.name + '.log', 'ab', 0)
+            self.f_err = open('/var/tmp/' + self.name + '.err', 'ab', 0)
 
+        self.dcontext = daemon.DaemonContext(pidfile=pid.PidFile(self.name, '/var/tmp',),
+                                             detach_process=self.detach_process,
+                                             stdout=self.f_log,
+                                             stderr=self.f_err)
         self.dcontext.signal_map = {
             signal.SIGTERM: self.exit_proc,
         }
 
         redef_print = kwargs.get('redef_print', False)
         self.print = builtins.print
-        if redef_print:
-            builtins.print = self.log_str
+        builtins.print = self.log_str
 
         with self.dcontext:
-            self.log_str('starting service: ' + self.name)
+            print('starting service: ' + self.name)
             self.pre_run()
             self.main()
+            notify('READY=1')
+            print('data loaded, going to main loop')
             self.run_main_loop()
 
     def exit_proc(self, signum, frame):
         self.log_str('signal recieved: %d, %s' % (signum, frame))
+        notify('STOPPING=1')
         self.clean_proc()
         self.quit_main_loop()
 
